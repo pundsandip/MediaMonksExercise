d256972f0d0de4ff057ca67207bc2c806a5ff416
Sandip Pund <pundsandip21@gmail.com>
2019-06-08 19:45:26 +0530

    Initial commit - project created

---
+++ MediaMonksExercise/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MediaMonksExercise
+//
+//  Created by Suresh Sagwal on 08/06/19.
+//  Copyright © 2019 AmpleSolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected Index: \(indexPath)")
+    }
+}
+
